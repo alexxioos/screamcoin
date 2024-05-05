1ba7d2fa1eae8ddf33cf70bb08595f942ded0cac
alexxioos <165800920+alexxioos@users.noreply.github.com>
2024-05-06 04:58:03 +0500

    Delete roll.js

--- roll.js
+++
@@ -1,9 +0,0 @@
-
-
-export function getroll (){
-    
-    var images = document.getElementsByClassName("imb");
-    images.forEach(function(img){
-      img.classList.add('rotate-vertical')  ;
-    });
-}
